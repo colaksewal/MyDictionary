237eb4d71cd8286ca43ca2f60c7bdcb854da6c6a
colaksewal <108463004+colaksewal@users.noreply.github.com>
2023-08-08 11:02:24 +0300

    Add project files.

---
+++ MyDictionary/MyDictionarys.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionarys<T>
+    {
+        T[] words;
+
+        public MyDictionarys()
+        {
+            words = new T[0];
+        }
+
+        public void Add(T value)
+        {
+            T[] tempWord = words;
+
+            words = new T[words.Length + 1 ];
+
+            for (int i = 0; i < tempWord.Length; i++)
+            {
+                words[i] = tempWord[i];
+            }
+
+            words[words.Length - 1 ] = value;
+        }
+
+        public int Lenght()
+        {
+            return words.Length;
+        }
+
+        public void PrintElement()
+        {
+            foreach (T value in words) {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyDictionary;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            MyDictionarys<string> myDictionarys = new MyDictionarys<string>();
+            myDictionarys.Add("one");
+            myDictionarys.Add("two");
+            myDictionarys.Add("three");
+
+            Console.WriteLine("Number of Elements: " + myDictionarys.Lenght());
+            myDictionarys.PrintElement();
+        
+        }
+    }
+}
